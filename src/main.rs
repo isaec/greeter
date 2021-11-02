6c2dc71c18af4633470bbad626d1134679e4c49f
Isaac <isaac@rubenfamily.com>
2021-11-02 10:50:06 -0700

    break reading sys into function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::fs;
+
+fn read_sys(path: &str) -> u16 {
+    fs::read_to_string(path)
+        .unwrap()
+        .trim()
+        .parse::<u16>()
+        .unwrap()
+}
+
 fn main() {
-    println!("greeting");
+    let sys_temp = read_sys("/sys/class/thermal/thermal_zone0/temp") / 1000; // celsius
+    let sys_batt_percent = read_sys("/sys/class/power_supply/BAT0/capacity"); // ideally would use BAT*
+    println!("temp: {}c\npercent: {}%", sys_temp, sys_batt_percent);
 }
