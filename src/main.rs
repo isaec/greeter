ac9f003e957640038ff1c3b5de051e5382c32572
Isaac <isaac@rubenfamily.com>
2021-11-02 22:28:34 -0700

    start work on uptime

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,19 @@ fn read_val_str(path: &str) -> String {
 
 fn main() {
     let sys_batt_percent = read_val("/sys/class/power_supply/BAT0/capacity"); // ideally would use BAT*
+    let uptime = {
+        let raw = fs::read_to_string("/proc/uptime").unwrap();
+        String::from(raw.split_whitespace().next().unwrap())
+    };
 
     println!(
         "running at {red}{sys_temp}c{reset}
 on {kernel_vers}
-percent: {}%",
-        sys_batt_percent,
+for {uptime}
+percent: {sys_batt_percent}%",
         sys_temp = read_val("/sys/class/thermal/thermal_zone0/temp") / 1000, // celsius
+        uptime = uptime,
+        sys_batt_percent = sys_batt_percent,
         kernel_vers = read_val_str("/proc/sys/kernel/osrelease"), // equivalent to uname -r
         red = color::Fg(color::Red),
         reset = color::Fg(color::Reset),
