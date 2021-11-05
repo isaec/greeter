a7bdb2ec4da2abffb92211a29eb355b761a1f7de
Isaac <isaac@rubenfamily.com>
2021-11-05 13:57:33 -0700

    use consts, get brightness

--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,14 @@ fn read_val_str(path: &str) -> String {
     String::from(fs::read_to_string(path).unwrap().trim())
 }
 
+const BAT_CAPACITY: &str = "/sys/class/power_supply/BAT0/capacity"; // ideally would use BAT*
+const ACTUAL_BACKLIGHT: &str = "/sys/class/backlight/amdgpu_bl0/actual_brightness";
+const MAX_BACKLIGHT: &str = "/sys/class/backlight/amdgpu_bl0/max_brightness";
+
+
 fn main() {
-    let sys_batt_percent = read_val("/sys/class/power_supply/BAT0/capacity"); // ideally would use BAT*
+    let sys_batt_percent = read_val(BAT_CAPACITY); 
+    let display_percent = ((read_val(ACTUAL_BACKLIGHT) as f32 / read_val(MAX_BACKLIGHT) as f32) * 100.0) as u8;
 
     let red_to_green = ColorRange::new(Color::rgb(230, 0, 115), Color::rgb(0, 175, 100));
     let blue_to_mag = ColorRange::new(Color::rgb(0, 175, 175), Color::rgb(230, 0, 115));
@@ -36,11 +42,13 @@ fn main() {
         "running at {red}{sys_temp}c{reset}
 on {kernel_vers}
 for {uptime}
+at {display_percent}% brightness
 with a xxx battery at {sys_batt_percent}%
 {batt_bar}
 ",
         sys_temp = read_val("/sys/class/thermal/thermal_zone0/temp") / 1000, // celsius
         uptime = uptime::get(),
+        display_percent = display_percent,
         sys_batt_percent = sys_batt_percent,
         batt_bar = bar::make(30, sys_batt_percent, &red_to_green, "<", "/", "-", ">"),
         kernel_vers = read_val_str("/proc/sys/kernel/osrelease"), // equivalent to uname -r
