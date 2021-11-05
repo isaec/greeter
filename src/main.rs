c047de7418c4aced4399d8a6580cb243c33a894f
Isaac <isaac@rubenfamily.com>
2021-11-05 15:47:01 -0700

    explain battery status

--- src/main.rs
+++ src/main.rs
@@ -21,20 +21,27 @@ fn read_val_str(path: &str) -> String {
 }
 
 const BAT_CAPACITY: &str = "/sys/class/power_supply/BAT0/capacity"; // ideally would use BAT*
+const BAT_STATUS: &str = "/sys/class/power_supply/BAT0/status";
 const ACTUAL_BACKLIGHT: &str = "/sys/class/backlight/amdgpu_bl0/actual_brightness";
 const MAX_BACKLIGHT: &str = "/sys/class/backlight/amdgpu_bl0/max_brightness";
 
 
 fn main() {
-    let sys_batt_percent = read_val(BAT_CAPACITY); 
+    let sys_batt_percent = read_val(BAT_CAPACITY);
+    // &fn[..] converts String to &str for easy matching
+    let batt_status = match &read_val_str(BAT_STATUS)[..] {
+        "Charging" => format!("charging battery at {}%", sys_batt_percent),
+        "Full" => format!("full battery"),
+        _ => format!("draining battery at {}%", sys_batt_percent), // draining
+    };
     let display_percent = ((read_val(ACTUAL_BACKLIGHT) as f32 / read_val(MAX_BACKLIGHT) as f32) * 100.0) as u16;
 
     let red_to_green = ColorRange::new(Color::rgb(230, 0, 115), Color::rgb(0, 175, 100));
-    let blue_to_mag = ColorRange::new(Color::rgb(0, 175, 175), Color::rgb(230, 0, 115));
+    let blue_to_mag = ColorRange::new(Color::rgb(0, 175, 230), Color::rgb(230, 0, 115));
 
     // for n in 0..=100 {
     //     if n % 2 == 0 {
-    //         println!("{}", bar::make(30, n, &red_to_green, "<", "/", "-", ">"));
+    //         println!("{}", bar::make(30, n, &blue_to_mag, "<", "/", "-", ">"));
     //     }
     // }
 
@@ -44,14 +51,14 @@ on {kernel_vers}
 for {uptime}
 at {display_percent}% brightness
 {display_bar}
-with a xxx battery at {sys_batt_percent}%
+with a {batt_status}
 {batt_bar}
 ",
         sys_temp = read_val("/sys/class/thermal/thermal_zone0/temp") / 1000, // celsius
         uptime = uptime::get(),
         display_percent = display_percent,
         display_bar = bar::make(20, display_percent, &blue_to_mag, "<", "/", "-", ">"),
-        sys_batt_percent = sys_batt_percent,
+        batt_status = batt_status,
         batt_bar = bar::make(30, sys_batt_percent, &red_to_green, "|", "=", "-", "|"),
         kernel_vers = read_val_str("/proc/sys/kernel/osrelease"), // equivalent to uname -r
         red = color::Fg(color::Red),
