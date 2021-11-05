c61da0c1073a9545047bb17b88ff1a6858f7e661
Isaac <isaac@rubenfamily.com>
2021-11-05 14:45:25 -0700

    brightness bar

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ const MAX_BACKLIGHT: &str = "/sys/class/backlight/amdgpu_bl0/max_brightness";
 
 fn main() {
     let sys_batt_percent = read_val(BAT_CAPACITY); 
-    let display_percent = ((read_val(ACTUAL_BACKLIGHT) as f32 / read_val(MAX_BACKLIGHT) as f32) * 100.0) as u8;
+    let display_percent = ((read_val(ACTUAL_BACKLIGHT) as f32 / read_val(MAX_BACKLIGHT) as f32) * 100.0) as u16;
 
     let red_to_green = ColorRange::new(Color::rgb(230, 0, 115), Color::rgb(0, 175, 100));
     let blue_to_mag = ColorRange::new(Color::rgb(0, 175, 175), Color::rgb(230, 0, 115));
@@ -43,14 +43,16 @@ fn main() {
 on {kernel_vers}
 for {uptime}
 at {display_percent}% brightness
+{display_bar}
 with a xxx battery at {sys_batt_percent}%
 {batt_bar}
 ",
         sys_temp = read_val("/sys/class/thermal/thermal_zone0/temp") / 1000, // celsius
         uptime = uptime::get(),
         display_percent = display_percent,
+        display_bar = bar::make(20, display_percent, &blue_to_mag, "<", "/", "-", ">"),
         sys_batt_percent = sys_batt_percent,
-        batt_bar = bar::make(30, sys_batt_percent, &red_to_green, "<", "/", "-", ">"),
+        batt_bar = bar::make(30, sys_batt_percent, &red_to_green, "|", "=", "-", "|"),
         kernel_vers = read_val_str("/proc/sys/kernel/osrelease"), // equivalent to uname -r
         red = color::Fg(color::Red),
         reset = color::Fg(color::Reset),
