fcda91f07b9f01475df7e03e0c710b759a232c56
Isaac <isaac@rubenfamily.com>
2021-11-16 09:48:43 -0800

    fix rare white cap

--- src/bar.rs
+++ src/bar.rs
@@ -78,7 +78,7 @@ pub fn make(
   let value_dec = value as f32 / 100.0;
 
   format!(
-    "{left_cap}{color}{full_chars}{reset}{empty_chars}{right_cap}",
+    "{reset}{left_cap}{color}{full_chars}{reset}{empty_chars}{right_cap}",
     full_chars = full_char.repeat((inner_chars * value_dec).ceil() as usize),
     empty_chars = empty_char.repeat((inner_chars * (1.0 - value_dec)).floor() as usize),
     left_cap = left_cap,
