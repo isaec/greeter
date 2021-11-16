25a426e6d939227046e29d0df0dd89c924d4f84e
Isaac <isaac@rubenfamily.com>
2021-11-16 09:53:51 -0800

    apply formatter, color percent w bar color

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 extern crate termion;
 
+use std::cmp::{max, min};
 use std::fs;
-use std::cmp::{min, max};
 use termion::color;
 
 mod bar;
 use bar::Color;
 use bar::ColorRange;
-mod uptime;
 mod audio;
+mod uptime;
 
 fn read_val(path: &str) -> u16 {
     fs::read_to_string(path)
@@ -30,36 +30,46 @@ const ACTUAL_BACKLIGHT: &str = "/sys/class/backlight/amdgpu_bl0/actual_brightnes
 const MAX_BACKLIGHT: &str = "/sys/class/backlight/amdgpu_bl0/max_brightness";
 
 fn main() {
-    let red_to_green = ColorRange::new3(Color::rgb(230, 0, 115), Color::rgb(230, 230, 60), Color::rgb(50, 200, 50));
+    let red_to_green = ColorRange::new3(
+        Color::rgb(230, 0, 115),
+        Color::rgb(230, 230, 60),
+        Color::rgb(50, 200, 50),
+    );
     let blue_to_mag = ColorRange::new2(Color::rgb(0, 175, 230), Color::rgb(230, 0, 115));
     let yellow_to_red = ColorRange::new2(Color::rgb(230, 230, 100), Color::rgb(230, 100, 150));
     let default = color::Fg(color::Rgb(150, 152, 150));
 
     let sys_batt_percent = read_val(BAT_CAPACITY);
     // &fn[..] converts String to &str for easy matching
-    let batt_status = match &read_val_str(BAT_STATUS)[..] {
-        "Charging" => format!(
-            "{c}charging battery{r} at {c}{}%{r}",
-            sys_batt_percent,
-            c = red_to_green.get_color(max(70, sys_batt_percent) as f32 / 100_f32),
-            r = default,
-        ),
-        "Full" => format!("{}full battery{}", red_to_green.get_color(1.0), default),
-        _ => format!(
-            "{c}draining battery{r} at {c}{}%{r}",
-            sys_batt_percent,
-            c = red_to_green.get_color(min(30, sys_batt_percent) as f32 / 100_f32),
-            r = default,
-        ),
+    let batt_status = {
+        let bat_color = red_to_green.get_color(sys_batt_percent as f32 / 100.0);
+        match &read_val_str(BAT_STATUS)[..] {
+            "Charging" => format!(
+                "{c}charging battery{r} at {bc}{}%{r}",
+                sys_batt_percent,
+                c = red_to_green.get_color(max(70, sys_batt_percent) as f32 / 100_f32),
+                bc = bat_color,
+                r = default,
+            ),
+            "Full" => format!("{}full battery{}", red_to_green.get_color(1.0), default),
+            _ => format!(
+                "{c}draining battery{r} at {bc}{}%{r}",
+                sys_batt_percent,
+                c = red_to_green.get_color(min(30, sys_batt_percent) as f32 / 100_f32),
+                bc = bat_color,
+                r = default,
+            ),
+        }
     };
-    let display_percent =
-        ((read_val(ACTUAL_BACKLIGHT) as f32 / read_val(MAX_BACKLIGHT) as f32) * 100.0).round() as u16;
+    let display_percent = ((read_val(ACTUAL_BACKLIGHT) as f32 / read_val(MAX_BACKLIGHT) as f32)
+        * 100.0)
+        .round() as u16;
 
     let (audio_level, audio_enabled) = audio::get();
 
     // for n in 0..=100 {
     //     if n % 2 == 0 {
-    //         println!("{} {}", bar::make(30, n, &red_to_green, "<", "/", "-", ">", &default), n);
+    //         println!("{} {}", bar::make(30, n, &blue_to_mag, "<", "/", "-", ">", &default), n);
     //     }
     // }
 
@@ -77,12 +87,39 @@ with a {batt_status}
         sys_temp = read_val("/sys/class/thermal/thermal_zone0/temp") / 1000, // celsius
         uptime = uptime::get(),
         display_percent = display_percent,
-        display_bar = bar::make(BAR_WIDTH, display_percent, &yellow_to_red, "<", "/", "-", ">", &default),
-        audio_bar = bar::make(BAR_WIDTH, audio_level, &blue_to_mag, "<", "\\", "-", ">", &default),
+        display_bar = bar::make(
+            BAR_WIDTH,
+            display_percent,
+            &yellow_to_red,
+            "<",
+            "/",
+            "-",
+            ">",
+            &default
+        ),
+        audio_bar = bar::make(
+            BAR_WIDTH,
+            audio_level,
+            &blue_to_mag,
+            "<",
+            "\\",
+            "-",
+            ">",
+            &default
+        ),
         audio_state = if audio_enabled { "on" } else { "muted" },
         audio_level = audio_level,
         batt_status = batt_status,
-        batt_bar = bar::make(30, sys_batt_percent, &red_to_green, "|", "=", "-", "|", &default),
+        batt_bar = bar::make(
+            30,
+            sys_batt_percent,
+            &red_to_green,
+            "|",
+            "=",
+            "-",
+            "|",
+            &default
+        ),
         kernel_vers = read_val_str("/proc/sys/kernel/osrelease"), // equivalent to uname -r
         default = default,
         reset = color::Fg(color::Reset),
