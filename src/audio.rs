528425392adb84d1dc800cd72e9ed66bf1a89912
Isaac <isaac@rubenfamily.com>
2021-12-31 00:19:49 -0800

    audio error handling

--- src/audio.rs
+++ src/audio.rs
@@ -1,23 +1,26 @@
 use std::process::Command;
 
 pub fn get() -> (u16, bool) {
-    let output = Command::new("amixer")
-        .args(["-D", "pulse", "get", "Master"])
-        .output()
-        .map(|o| String::from_utf8_lossy(&o.stdout).trim().to_owned())
-        .unwrap();
+    fn work() -> Option<(u16, bool)> {
+        let output = Command::new("amixer")
+            .args(["-D", "pulse", "get", "Master"])
+            .output()
+            .map(|o| String::from_utf8_lossy(&o.stdout).trim().to_owned())
+            .ok()?;
 
-    let last_line = &output.lines().last().unwrap();
+        let last_line = &output.lines().last()?;
 
-    let data = last_line
-        .split_whitespace()
-        .filter(|x| x.starts_with('[') && !x.contains("dB"))
-        .map(|x| x.trim_matches(&['[', ']', '%'] as &[_]))
-        .collect::<Vec<&str>>();
+        let data = last_line
+            .split_whitespace()
+            .filter(|x| x.starts_with('[') && !x.contains("dB"))
+            .map(|x| x.trim_matches(&['[', ']', '%'] as &[_]))
+            .collect::<Vec<&str>>();
 
-    let volume = data.get(0).unwrap().parse::<u16>().unwrap();
+        let volume = data.get(0)?.parse::<u16>().ok()?;
 
-    let enabled = *data.get(1).unwrap() == "on";
+        let enabled = *data.get(1)? == "on";
 
-    (volume, enabled)
+        Some((volume, enabled))
+    }
+    work().unwrap_or((0_u16, false))
 }
